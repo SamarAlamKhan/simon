f1fb678dd9c9efb83fd44fe10cde2858de87947e
SamarAlamKhan <alamkhansamar@gmail.com>
2025-10-30 17:26:07 +0530

    Improve game start handling and mobile support
    Refactor game start logic and mobile detection.

--- index.js
+++ index.js
@@ -3,12 +3,38 @@ let userPattern = [];
 let level = 0;
 let gameStart = false;
 
+$(document).ready(() => {
+    const deviceMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+    if (!deviceMobile) {
+        $("h1").html("Press Any Key To Start");
+    } else {
+        $("h1").html("Press 'START' To Play")
+    }
+})
+
+function condition() {
+    if (!deviceMobile) {
+        $("h1").html("Press Any Key To Start");
+    } else {
+        $("h1").html("Press 'START' To Play")
+    }
+}
+
 $(document).keydown(() => {
+    start();
+});
+
+$("button").click(() => {
+    start();
+});
+
+let start = () => {
     if (!gameStart) {
         nextSequence();
         gameStart = true;
     };
-});
+}
 
 let nextSequence = () => {
     userPattern = [];
@@ -52,7 +78,16 @@ let checkAnswer = (currentLevel) => {
         new Audio("./sounds/wrong.mp3").play();
         $("body").css("backgroundColor", "red");
         setTimeout(() => {
-            $("h1").html(`GAME OVER<br><br>Press Any Key To Start Again`);
+            $(document).ready(() => {
+                const deviceMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+                if (!deviceMobile) {
+                    $("h1").html("GAME OVER<br><br>Press Any Key To Play Again");
+                } else {
+                    $("h1").html("GAME OVER<br><br>Press 'START' To Play Again");
+                }
+            });
+
             $("body").css("backgroundColor", "#011F3F");
         }, 300);
     
@@ -64,5 +99,4 @@ let reset = () => {
     level = 0;
     gamePattern = [];
     gameStart = false;
-
 }
